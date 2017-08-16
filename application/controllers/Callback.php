1e5d88b3e56b60ff88955bf48a102723112d463c
thanhlap <thanhlap135@gmail.com>
2017-08-16 13:54:51 +0700

    sua file

--- application/controllers/Callback.php
+++ application/controllers/Callback.php
@@ -95,7 +95,7 @@ class Callback extends CI_Controller {
 							array('type' => 'text', 'text' => $replyMsg), 
 							array('type' => 'text', 'text' => 'step ' . $step)
 						);
-						if ($result['result'] == "true" && $replyMsg == '確認') {
+						if ($result['result'] == "true" && $message->{"text"} == '確認') {
 							$data_chat['step'] = 4;
 							$messageData = array(
 								array('type' => 'text', 'text' => $replyMsg), 
