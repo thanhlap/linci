6e6d7a10373ffe519aabbe9bc5283eedf07f2f2d
thanhlap <thanhlap135@gmail.com>
2017-08-16 15:53:04 +0700

    sua file7

--- application/controllers/Callback.php
+++ application/controllers/Callback.php
@@ -107,15 +107,15 @@ class Callback extends CI_Controller {
 					
 					break;
 				case 3:
-						$list = $this->eyelash_api->list($lastMsg['message_ref'] ,$message->{"text"});
-						$data_chat['message_ref'] = 'mobile: ' . $lastMsg['message_ref'] . 'password: ' . $message->{"text"};
-						if($list['list'] == "true"){
-							$data_chat['step'] = 4;
-							$messageData = array(
-								array('type' => 'text', 'text' => $list), 
-								array('type' => 'text', 'text' => 'step ' . $step)
-							);
-						}
+						// $list = $this->eyelash_api->list($lastMsg['message_ref'] ,$message->{"text"});
+						// $data_chat['message_ref'] = 'mobile: ' . $lastMsg['message_ref'] . 'password: ' . $message->{"text"};
+						// if($list['list'] == "true"){
+						// 	$data_chat['step'] = 4;
+						// 	$messageData = array(
+						// 		array('type' => 'text', 'text' => $list), 
+						// 		array('type' => 'text', 'text' => 'step ' . $step)
+						// 	);
+						// }
 
 					break;
 				default:
