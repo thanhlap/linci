2fc9459721bae367387d4474848feee948ede316
thanhlap <thanhlap135@gmail.com>
2017-08-16 11:42:19 +0700

    sua file

--- application/controllers/Callback.php
+++ application/controllers/Callback.php
@@ -76,7 +76,7 @@ class Callback extends CI_Controller {
 
 					$messageData = array(
 						array('type' => 'text', 'text' => $replyMsg),
-						// array('type' => 'text', 'text' => 'step ' . $step)
+						array('type' => 'text', 'text' => 'step ' . $step)
 					);
 					
 					break;
@@ -91,7 +91,7 @@ class Callback extends CI_Controller {
 						$replyMsg = '店舗を入力してください。';
 						$messageData = array(
 							array('type' => 'text', 'text' => $replyMsg), 
-							// array('type' => 'text', 'text' => 'step ' . $step)
+							array('type' => 'text', 'text' => 'step ' . $step)
 						);
 						
 					}else{
