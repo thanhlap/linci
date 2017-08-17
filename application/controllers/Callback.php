a3fd218800052263dc1912241d7e96d982477dc6
thanhlap <thanhlap135@gmail.com>
2017-08-17 13:16:21 +0700

    fix file

--- application/controllers/Callback.php
+++ application/controllers/Callback.php
@@ -134,32 +134,37 @@ class Callback extends CI_Controller {
 								//array('type' => 'text', 'text' => $replyMsg), 
 								array('type' => 'text', 'text' => 'step ' . $step),
 								array(
-							            	"type"=> "template",
-										  //	"altText"=> "this is a buttons template",
-										  	"template"=> array(
-										      	"type"=> "buttons",
-										      	// "thumbnailImageUrl"=> "https://example.com/bot/images/image.jpg",
-										      	// "title"=> "Menu",
-										      	// "text"=> "Please select",
-										      	"actions"=> array(
-											          // array(
-											          //   "type"=> "postback",
-											          //   "label"=> "Buy",
-											          //   "data"=> "action=buy&itemid=123"
-											          // ),
-											          // array(
-											          //   "type"=> "postback",
-											          //   "label"=> "Add to cart",
-											          //   "data"=> "action=add&itemid=123"
-											          // ),
-											          array(
-											            "type"=> "uri",
-											            "label"=> "View detail",
-											            "uri"=> "http://example.com/page/123"
-											          )
-										     	)
-										  	)
-							        )
+									 	"type"=> "video",
+									    "originalContentUrl"=> "https://example.com/original.mp4",
+									    "previewImageUrl"=> "https://example.com/preview.jpg"
+								)
+								// array(
+							 //            	"type"=> "template",
+								// 		  //	"altText"=> "this is a buttons template",
+								// 		  	"template"=> array(
+								// 		      	"type"=> "buttons",
+								// 		      	// "thumbnailImageUrl"=> "https://example.com/bot/images/image.jpg",
+								// 		      	// "title"=> "Menu",
+								// 		      	// "text"=> "Please select",
+								// 		      	"actions"=> array(
+								// 			          // array(
+								// 			          //   "type"=> "postback",
+								// 			          //   "label"=> "Buy",
+								// 			          //   "data"=> "action=buy&itemid=123"
+								// 			          // ),
+								// 			          // array(
+								// 			          //   "type"=> "postback",
+								// 			          //   "label"=> "Add to cart",
+								// 			          //   "data"=> "action=add&itemid=123"
+								// 			          // ),
+								// 			          array(
+								// 			            "type"=> "uri",
+								// 			            "label"=> "View detail",
+								// 			            "uri"=> "http://example.com/page/123"
+								// 			          )
+								// 		     	)
+								// 		  	)
+							 //        )
 							);
 						}
 					break;
