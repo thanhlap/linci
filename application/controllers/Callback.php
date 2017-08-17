eaaf68d5320ea9cadf8ba632dbc1a9dcc9c0db17
thanhlap <thanhlap135@gmail.com>
2017-08-17 10:04:43 +0700

    fix file

--- application/controllers/Callback.php
+++ application/controllers/Callback.php
@@ -130,25 +130,35 @@ class Callback extends CI_Controller {
 						$data_chat['step'] = 4;
 						$replyMsg = 'test';
 						$messageData = array(
-							array('type' => 'buttons', 'text' => $replyMsg), 
+							//array('type' => 'text', 'text' => $replyMsg), 
 							array('type' => 'text', 'text' => 'step ' . $step),
-							// array(
-						 //            'type' => 'buttons',
-						 //            'title' => 'test',
-						 //            'text' => $replyMsg,
-						 //            'actions' => array(
-						 //                array(
-						 //                    'type' => 'postback',
-						 //                    'label' => 'webhookにpost送信',
-						 //                    'data' => 'value'
-						 //                ),
-						 //                array(
-						 //                    'type' => 'uri',
-						 //                    'label' => 'googleへ移動',
-						 //                    'uri' => 'https://google.com'
-						 //                )
-						 //            )
-						 //        )
+							array(
+						            "type"=> "template",
+									  "altText"=> "this is a buttons template",
+									  "template"=> {
+									      "type"=> "buttons",
+									      "thumbnailImageUrl"=> "https://example.com/bot/images/image.jpg",
+									      "title"=> "Menu",
+									      "text"=> "Please select",
+									      "actions"=> [
+									          {
+									            "type"=> "postback",
+									            "label"=> "Buy",
+									            "data"=> "action=buy&itemid=123"
+									          },
+									          {
+									            "type"=> "postback",
+									            "label"=> "Add to cart",
+									            "data"=> "action=add&itemid=123"
+									          },
+									          {
+									            "type"=> "uri",
+									            "label"=> "View detail",
+									            "uri"=> "http://example.com/page/123"
+									          }
+									      ]
+									  }
+						        )
 						);
 					break;
 				
