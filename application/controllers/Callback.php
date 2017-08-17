c4eb5d678d2ab3bdb409dace71fd2d7d3e207ca3
thanhlap <thanhlap135@gmail.com>
2017-08-17 13:31:57 +0700

    fix file7

--- application/controllers/Callback.php
+++ application/controllers/Callback.php
@@ -146,9 +146,9 @@ class Callback extends CI_Controller {
 										"type"=> "location",
 									    "title"=> "my location",
 									    "address"=> "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-									    "latitude"=> 35.65910807942215,
-									    "longitude"=> 139.70372892916203
-								);
+									    "latitude"=> "35.65910807942215",
+									    "longitude"=> "139.70372892916203"
+								)
 								// array(
 							 //            	"type"=> "template",
 								// 		  //	"altText"=> "this is a buttons template",
