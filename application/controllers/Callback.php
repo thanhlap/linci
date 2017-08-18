ce91c089ba111f8a8f5938b2fa251e18d4b4e2bc
thanhlap <thanhlap135@gmail.com>
2017-08-18 13:44:08 +0700

    upload noi dung

--- application/controllers/Callback.php
+++ application/controllers/Callback.php
@@ -128,78 +128,78 @@ class Callback extends CI_Controller {
 						}
 					break;
 					
-	// 				case 3:
-	// 					$replyMsg = '店舗を入力してください。';
-	// 					$listStores = 'Have not any store.';
-	// 					$results = $this->eyelash_api->listStore();
-	// 					if ($results != null){
-	// 						$stores = $results["response"]["Items"]["Item"];
-	// 						if(($stores != NULL) && (count($stores) > 0)){
-	// 							$arrStores = array();
-	// 							$listStores = '';
-	// 							foreach ($stores as $store){
-	// 								if (strpos($store['store_name'], $message_text) !== false) {
-	// 									$arrStores[] = $store;
-	// 									if ($listStores != '')
-	// 										$listStores .= $store['store_name'];
-	// 										$listStores .= "\n\n";
-	// 								}
-	// 							}
-	// 							//$messageData = array(array('type' => 'text', 'text' => $replyMsg), array('type' => 'text', 'text' => $listStores));
-	// 							if (count($arrStores) > 4){
-	// 								$data_chat['step'] = 3;
-	// 								$lastOrder['step'] = 3;
-	// 								$messageData = array(
-	// 									array('type' => 'text', 'text' => $replyMsg), 
-	// 									array('type' => 'text', 'text' => $listStores));
-	// 							}else{
-	// 								$data_chat['step'] = 4;
-	// 								$lastOrder['step'] = 4;
-	// 								$arrActions = array();
-	// 								for ($i = 0; $i < count($arrStores); $i++){
-	// 									$action = array();
-	// 									$action['type'] = 'postback';
-	// 									$action['label'] = $arrStores[$i]['store_name'];
-	// 									$action['data'] = 'key=store&value=' . $arrStores[$i]['store_id'];
-	// 									//$action['data'] = $arrStores[$i]['store_id'];
-	// 									$action['text'] = $arrStores[$i]['store_name'];
-	// 									$arrActions[] = $action;
-	// 								}
-	// 								//ボタンタイプ
-	// 								$messageData = [array(
-	// 										'type' => 'template',
-	// 										'altText' => $replyMsg,
-	// 										'template' => array(
-	// 												'type' => 'buttons',
-	// 												'title' => '店舗',
-	// 												'text' => '選択してね',
-	// 												'actions' => $arrActions
-	// 										)
-	// 								)];
-	// 							}
-	// 						}
-	// 					}else{
-	// 						$messageData = array(
-	// 							array('type' => 'text', 'text' => $replyMsg),
-	// 							array('type' => 'text', 'text' => $listStores));
-	// 					}
-					
-	// 				break;
-					case 4:
-						if ($message_type == 'postback'){
-							$data_chat['step'] = 5;
-							$data_chat['message_ref'] = $store_id;
-	// 						$store_id = $jsonObj->{"events"}[0]->{"postback"};
-	// 						$store_id = $store_id->{"data"};
-							$dataPB = $jsonObj->{"events"}[0]->{"postback"};
-							$dataPB = $dataPB->{"data"};
-							//$this->saveLog("dataPB", $dataPB);
-							parse_str($dataPB, $postbackData);
-							//$this->saveLog("store_id", $postbackData['value']);
-							$lastOrder['step'] = 5;
-							$lastOrder['store_id'] = $postbackData['value'];
+					case 3:
+						$replyMsg = '店舗を入力してください。';
+						$listStores = 'Have not any store.';
+						$results = $this->eyelash_api->listStore();
+						if ($results != null){
+							$stores = $results["response"]["Items"]["Item"];
+							if(($stores != NULL) && (count($stores) > 0)){
+								$arrStores = array();
+								$listStores = '';
+								foreach ($stores as $store){
+									if (strpos($store['store_name'], $message_text) !== false) {
+										$arrStores[] = $store;
+										if ($listStores != '')
+											$listStores .= $store['store_name'];
+											$listStores .= "\n\n";
+									}
+								}
+								//$messageData = array(array('type' => 'text', 'text' => $replyMsg), array('type' => 'text', 'text' => $listStores));
+								if (count($arrStores) > 4){
+									$data_chat['step'] = 3;
+									$lastOrder['step'] = 3;
+									$messageData = array(
+										array('type' => 'text', 'text' => $replyMsg), 
+										array('type' => 'text', 'text' => $listStores));
+								}else{
+									$data_chat['step'] = 4;
+									$lastOrder['step'] = 4;
+									$arrActions = array();
+									for ($i = 0; $i < count($arrStores); $i++){
+										$action = array();
+										$action['type'] = 'postback';
+										$action['label'] = $arrStores[$i]['store_name'];
+										$action['data'] = 'key=store&value=' . $arrStores[$i]['store_id'];
+										//$action['data'] = $arrStores[$i]['store_id'];
+										$action['text'] = $arrStores[$i]['store_name'];
+										$arrActions[] = $action;
+									}
+									//ボタンタイプ
+									$messageData = [array(
+											'type' => 'template',
+											'altText' => $replyMsg,
+											'template' => array(
+													'type' => 'buttons',
+													'title' => '店舗',
+													'text' => '選択してね',
+													'actions' => $arrActions
+											)
+									)];
+								}
+							}
+						}else{
+							$messageData = array(
+								array('type' => 'text', 'text' => $replyMsg),
+								array('type' => 'text', 'text' => $listStores));
 						}
+					
 					break;
+	// 				case 4:
+	// 					if ($message_type == 'postback'){
+	// 						$data_chat['step'] = 5;
+	// 						$data_chat['message_ref'] = $store_id;
+	// // 						$store_id = $jsonObj->{"events"}[0]->{"postback"};
+	// // 						$store_id = $store_id->{"data"};
+	// 						$dataPB = $jsonObj->{"events"}[0]->{"postback"};
+	// 						$dataPB = $dataPB->{"data"};
+	// 						//$this->saveLog("dataPB", $dataPB);
+	// 						parse_str($dataPB, $postbackData);
+	// 						//$this->saveLog("store_id", $postbackData['value']);
+	// 						$lastOrder['step'] = 5;
+	// 						$lastOrder['store_id'] = $postbackData['value'];
+	// 					}
+	// 				break;
 
 	// 				case 5:
 	// 					if ($message_type == 'message'){
